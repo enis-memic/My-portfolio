ec4553b0ae4ed69cd91c316a2b69822050a586b6
Enis <enis.mema@gmail.com>
2023-01-31 12:15:11 +0100

    Change of works section, delete html version and implemented object with javascript

---
+++ data.js
@@ -0,0 +1,56 @@
+const projects = [
+  {
+    name: 'Tonic',
+    client: 'Canopy',
+    id: 1,
+    year: 2015,
+    img: './images/Snapshoot Portfolio.png',
+    technologies: ['html', 'css', 'javascript'],
+    paragraph:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum',
+    liveVersion: 'https://github.com/enis-memic',
+    source: 'https://github.com/enis-memic',
+  },
+  {
+    name: 'Multi-Post Stories',
+    client: 'Canopy',
+    id: 2,
+    year: 2015,
+    img: './images/Snapshoot Portfolio2.png',
+    technologies: ['html', 'css', 'javascript'],
+    paragraph:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum',
+    liveVersion: 'https://github.com/enis-memic',
+    source: 'https://github.com/enis-memic',
+  },
+  {
+    name: 'Tonic',
+    client: 'Canopy',
+    id: 3,
+    year: 2015,
+    img: './images/Snapshoot Portfolio3.png',
+    technologies: ['html', 'css', 'javascript'],
+    paragraph:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum',
+    liveVersion: 'https://github.com/enis-memic',
+    source: 'https://github.com/enis-memic',
+  },
+  {
+    name: 'Multi-Post Stories',
+    client: 'Canopy',
+    id: 4,
+    year: 2015,
+    img: './images/Snapshoot Portfolio4.png',
+    technologies: ['html', 'css', 'javascript'],
+    paragraph:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum',
+    liveVersion: 'https://github.com/enis-memic',
+    source: 'https://github.com/enis-memic',
+  },
+];
+
+export default projects;
