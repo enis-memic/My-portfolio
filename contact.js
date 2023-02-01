6d9518c96b247fe60f522806e7eeb9a43fc6c3d9
Enis <enis.mema@gmail.com>
2023-02-01 14:04:25 +0100

    Fix linters

--- contact.js
+++ contact.js
@@ -1,14 +1,11 @@
-const submit = document
-  .querySelector('.submit')
-  .addEventListener('click', (e) => {
-    const email = document.getElementById('email').value;
-    const errorEmail = document.querySelector('.error');
+document.querySelector('.submit').addEventListener('click', (e) => {
+  const email = document.getElementById('email').value;
+  const errorEmail = document.querySelector('.error');
 
-    const right = (str) => str === str.toLowerCase();
+  const right = (str) => str === str.toLowerCase();
 
-    if (!right(email)) {
-      e.preventDefault();
-      errorEmail.textContent =
-        'Please check if your email is in lowercase letters!!!';
-    }
-  });
+  if (!right(email)) {
+    e.preventDefault();
+    errorEmail.textContent = 'Please check if your email is in lowercase letters!!!';
+  }
+});
