57536842206c10602fe1267dcb517fad1a0e8546
Enis <enis.mema@gmail.com>
2023-02-28 16:17:54 +0100

    Add of a project

--- data.js
+++ data.js
@@ -1,17 +1,17 @@
 const projects = [
   {
-    name: 'Tonic',
-    client: 'Canopy',
+    name: 'Awesome Books ES6',
+    client: 'Microverse',
     id: 1,
-    year: 2015,
-    img: './images/Snapshoot Portfolio.png',
+    year: 2023,
+    img: './images/project1-1',
     technologies: ['html', 'css', 'javascript'],
     paragraph:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    liveVersion: 'https://github.com/enis-memic',
-    source: 'https://github.com/enis-memic',
+      'This is the website of a book-list where you can add and remove books with their title and authour. So it is basically page for you to keep track of books you like or you have read!',
+    liveVersion: 'https://enis-memic.github.io/Awesome-books-es6/',
+    source: 'https://github.com/enis-memic/Awesome-books-es6',
   },
   {
     name: 'Multi-Post Stories',
